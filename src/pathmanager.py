5a555eb5340d420bcf3d5d6087f17aae43315d9f
Mohamed Lamine KARTOBI <chakibdace@gmail.com>
2024-07-23 11:25:42 +0200

    Apply class method convention

--- src/pathmanager.py
+++ src/pathmanager.py
@@ -5,31 +5,31 @@ DEFAULT_DIR_PATH = Path(__file__).resolve().parent.parent
 class PathManager: 
     # Maps
     @classmethod
-    def map_path(self, map):
+    def map_path(cls, map):
         return DEFAULT_DIR_PATH / f"Graphics/maps/{map}.tmx"
 
     # Assets Config JSON
     @classmethod
-    def character_json_path(self, character_name):
+    def character_json_path(cls, character_name):
         return DEFAULT_DIR_PATH  / f"Config/{character_name}/Base/{character_name}.json"
     
     # Menu
     @classmethod
-    def menu_image_path(self, image_name):
+    def menu_image_path(cls, image_name):
         return DEFAULT_DIR_PATH  / f"Graphics/menu/{image_name}"
     
     # Music theme
     @classmethod
-    def soundtrack(self, name):
+    def soundtrack(cls, name):
         return DEFAULT_DIR_PATH  / f"Sounds/{name}.wav"
     
     # Dialog
     @classmethod
-    def dialogFile(self, filename):
+    def dialogFile(cls, filename):
         return DEFAULT_DIR_PATH / f"Graphics/dialog/{filename}"
     
     @classmethod
-    def dir(self):
+    def dir(cls):
         return DEFAULT_DIR_PATH
         
     
